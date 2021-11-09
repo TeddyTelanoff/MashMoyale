25ee87a3ea32da508f109ab8c7d7fa3ade5b9e92
Treidex <treidexwastaken@gmail.com>
2021-11-08 17:50:45 -0800

    .

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -9,17 +9,17 @@ public class Tower : MonoBehaviour
     public TextMeshPro hpText;
     public float maxHp;
     public float hp {
-        get => _hp;
+        get => _health;
         set
         {
             if (value <= 0)
                 Destroy(gameObject);
 
-            _hp = value;
-            hpText.text = $"{(int) _hp}/{(int) maxHp}hp";
+            _health = value;
+            hpText.text = $"{(int) _health}/{(int) maxHp}hp";
 		}
     }
 
     [SerializeField]
-    private float _hp;
+    private float _health;
 }
